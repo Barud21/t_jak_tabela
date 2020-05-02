b7279c872cbb9ac612a10bfde15e2664c4f13c4e
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-05-02 20:06:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import sqlite3
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    app.db_connection = sqlite3.connect('chinook.db')
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    app.db_connection.close()
