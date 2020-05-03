7291e1a020c703b49d16483edbdaf9ea8bb84ccd
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-05-03 12:42:00 +0200

    Wykład 4, z01 - code v02

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import aiosqlite
 import sqlite3
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    app.db_connection = sqlite3.connect('chinook.db')
+    app.db_connection = await aiosqlite.connect('chinook.db')
 
 @app.on_event("shutdown")
 async def shutdown():
@@ -21,10 +22,8 @@ async def root():
 
 @app.get("/tracks")
 async def get_tracks(page: int = 0, per_page: int = 10):
-    app.db_connection.row_factory = sqlite3.Row
-    cursor = app.db_connection.execute("SELECT TrackId, Name, AlbumId, MediaTypeId, GenreId, "
-                                       "Composer, Milliseconds, Bytes, UnitPrice "
-                                       "FROM tracks ORDER_BY TrackId LIMIT :per_page "
-                                       "OFFSET :per_page* :page", {'per_page': per_page, 'page': page})
+    app.db_connection.row_factory = aiosqlite.Row
+    cursor = await app.db_connection.execute("SELECT * FROM tracks ORDER BY TrackId LIMIT :per_page"
+                                       "OFFSET :per_page*:page", {'page': page, 'per_page': per_page})
     tracks = await cursor.fetchall()
     return tracks
