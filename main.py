71f27a3f5bd89abb85dde46263d45dd6a57ec368
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-05-03 13:39:52 +0200

    Wykład 4, z02 - code v04

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import aiosqlite
 import sqlite3
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 
 app = FastAPI()
 
@@ -20,6 +20,7 @@ async def root():
     return {"data": data}
 
 
+### ZADANIE 1 #########################################################
 @app.get("/tracks")
 async def get_tracks(page: int = 0, per_page: int = 10):
     app.db_connection.row_factory = aiosqlite.Row
@@ -28,3 +29,17 @@ async def get_tracks(page: int = 0, per_page: int = 10):
         {'page': page, 'per_page': per_page})
     tracks = await cursor.fetchall()
     return tracks
+
+
+### ZADANIE 2 #########################################################
+@app.get("/tracks/composers")
+async def composer_tracks(response: Response, composer_name: str):
+    app.db_connection.row_factory = aiosqlite.Row
+    cursor = await app.db_connection.execute(
+        "SELECT Name FROM tracks WHERE Composer = :composer_name ORDER BY Name",
+        {'composer_name': composer_name})
+    composer = await cursor.fetchall()
+    if len(composer) == 0:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail": {"error": "There is no such composer in database"}}
+    return composer
